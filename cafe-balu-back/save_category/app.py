c7a807b75cb608254e68ea1dfabdd9a4a190c6c0
Lisseth Fuentes <20211tn014@utez.edu.mx>
2024-06-07 00:42:51 -0600

    Save category

---
+++ cafe-balu-back/save_category/app.py
@@ -0,0 +1,88 @@
+import json
+import pymysql
+import logging
+
+rds_host = "database-cafe-balu.cziym6ii4nn7.us-east-2.rds.amazonaws.com"
+rds_user = "baluroot"
+rds_password = "baluroot"
+rds_db = "cafe_balu"
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, __):
+    try:
+        if 'pathParameters' not in event:
+            logger.error("pathParameters not found in the event")
+            raise KeyError('pathParameters')
+
+        newName = event['pathParameters'].get('newName')
+        id = event['pathParameters'].get('id')
+
+        if newName is None or id is None:
+            logger.warning("Missing fields: id or newName")
+            return {
+                "statusCode": 400,
+                "body": json.dumps({
+                    "message": "MISSING_FIELDS"
+                }),
+            }
+
+        update_category(id, newName)
+
+        logger.info("Category updated successfully: id=%s, newName=%s", id, newName)
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "CATEGORY_UPDATED",
+            }),
+        }
+    except KeyError as e:
+        logger.error("Missing key in event: %s", str(e))
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "MISSING_KEY",
+                "error": str(e)
+            }),
+        }
+    except Exception as e:
+        logger.error("Internal server error: %s", str(e))
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": "INTERNAL_SERVER_ERROR",
+                "error": str(e)
+            }),
+        }
+
+
+def update_category(id, newName):
+    connection = pymysql.connect(host=rds_host, user=rds_user, password=rds_password, db=rds_db)
+    print(connection)
+    try:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("UPDATE categories SET name = %s WHERE id = %s", (newName, id))
+            connection.commit()
+            logger.info("Database updated successfully for id=%s", id)
+        except Exception as e:
+            logger.error("Database update error: %s", str(e))
+            return {
+                "statusCode": 500,
+                "body": json.dumps({
+                    "message": "DATABASE_ERROR"
+                }),
+            }
+    except Exception as e:
+        logger.error("Database connection error: %s", str(e))
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": "CONNECTION_ERROR"
+            }),
+        }
+    finally:
+        connection.close()

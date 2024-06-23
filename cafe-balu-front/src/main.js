69abcbb9aa96d54f6b0c7ed67c0de9a218a89849
Marianne <20213tn140@utez.edu.mx>
2024-06-23 12:57:23 -0600

    add bootstrap config

--- cafe-balu-front/src/main.js
+++ cafe-balu-front/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+// BootstrapVue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   render: (h) => h(App)

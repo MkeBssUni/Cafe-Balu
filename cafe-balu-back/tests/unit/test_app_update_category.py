6cc0c2b60d500e78bdd525e2cc1235d8ec853cb7
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-06-24 09:15:49 -0600

    [ADD] unit test for update category

--- cafe-balu-back/tests/unit/test_app_update_category.py
+++ cafe-balu-back/tests/unit/test_app_update_category.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 mock_name_duplicated = {
     "body": json.dumps({
         "id": 1,
-        "name": "Nombre duplicado"
+        "name": "Pasteles"
     })
 }
 
@@ -17,16 +17,36 @@ mock_category_not_exists = {
     })
 }
 
+mock_success ={
+    "body": json.dumps({
+        "id": 1,
+        "name": "Test update category success"
+    })
+}
+
+mock_missing_fields ={
+    "body": json.dumps({
+        "id": None,
+        "name": "Nombre"
+    })
+}
+
+mock_empty_fields ={
+    "body": json.dumps({
+        "id": 1,
+        "name": ""
+    })
+}
+
 class TestUpdateCategory(unittest.TestCase):
 
     @patch("update_category.app.category_exist")
     @patch("update_category.app.duplicated_name")
     def test_lambda_category_not_exists(self, mock_category_exist, mock_duplicated_name):
-        mock_category_exist.return_value = True
-        mock_duplicated_name.return_value = True
+        mock_category_exist.return_value = False
+        mock_duplicated_name.return_value = False
 
         result = app.lambda_handler(mock_category_not_exists, None)
-        print(result)
         status_code = result["statusCode"]
         self.assertEqual(status_code, 404)
         body = json.loads(result["body"])
@@ -37,12 +57,38 @@ class TestUpdateCategory(unittest.TestCase):
     @patch("update_category.app.duplicated_name")
     def test_lambda_duplicated_name(self, mock_category_exist, mock_duplicated_name):
         mock_category_exist.return_value = True
-        mock_duplicated_name.return_value = False
+        mock_duplicated_name.return_value = True
 
         result = app.lambda_handler(mock_name_duplicated, None)
-        print(result)
         status_code = result["statusCode"]
-        self.assertEqual(status_code, 404)
+        self.assertEqual(status_code, 400)
         body = json.loads(result["body"])
         self.assertIn("message", body)
         self.assertEqual(body["message"], "DUPLICATED_NAME")
+
+    @patch("update_category.app.duplicated_name")
+    @patch("update_category.app.category_exist")
+    def test_lambda_success(self, mock_category_exist, mock_duplicated_name):
+        mock_duplicated_name.return_value = False
+        mock_category_exist.return_value = True
+
+        result = app.lambda_handler(mock_success, None)
+        status_code = result["statusCode"]
+        self.assertEqual(status_code, 200)
+        body = json.loads(result["body"])
+        self.assertIn("message", body)
+        self.assertEqual(body["message"], "CATEGORY_UPDATED")
+
+    @patch("update_category.app.duplicated_name")
+    @patch("update_category.app.category_exist")
+    def test_lambda_missing_fields(self, mock_category_exist, mock_duplicated_name):
+        mock_duplicated_name.return_value = False
+        mock_category_exist.return_value = True
+
+        result = app.lambda_handler(mock_empty_fields, None)
+        status_code = result["statusCode"]
+        self.assertEqual(status_code, 400)
+        body = json.loads(result["body"])
+        self.assertIn("message", body)
+        self.assertEqual(body["message"], "EMPTY_FIELDS")
+

542cb201d833ed92fac011ae706ab6476f289f94
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-07-06 15:19:28 -0600

    [ADD] unit test for new password lambda

--- cafe-balu-back/tests/unit/test_app_reset_password.py
+++ cafe-balu-back/tests/unit/test_app_reset_password.py
@@ -1,6 +1,8 @@
-import unittest
 import json
+import unittest
 from newPassword import app
+from unittest.mock import patch
+from botocore.exceptions import ClientError
 
 mock_success = {
     "body": json.dumps({
@@ -10,9 +12,58 @@ mock_success = {
     })
 }
 
-class TestLogin(unittest.TestCase):
-    def test_login_success(self):
+
+class TestResetPassword(unittest.TestCase):
+    @patch("newPassword.app.boto3.client")
+    def test_new_password_success(self, mock_boto_client):
+        mock_client_instance = mock_boto_client.return_value
+        mock_client_instance.admin_initiate_auth.return_value = {
+            "ChallengeName": "NEW_PASSWORD_REQUIRED",
+            "Session": "session"
+        }
         result = app.lambda_handler(mock_success, None)
         status_code = result["statusCode"]
         self.assertEqual(status_code, 200)
-        body = json.loads(result["body"])
+
+    @patch("newPassword.app.boto3.client")
+    def test_unexpected_challege(self, mock_boto_client):
+        mock_client_instance = mock_boto_client.return_value
+        mock_client_instance.admin_initiate_auth.return_value = {
+            "ChallengeName": "UNEXPECTED_CHALLENGE",
+            "Session": "session"
+        }
+        result = app.lambda_handler(mock_success, None)
+        status_code = result["statusCode"]
+        self.assertEqual(status_code, 400)
+
+    @patch("newPassword.app.boto3.client")
+    def test_client_error(self, mock_boto_client):
+        mock_client_instance = mock_boto_client.return_value
+        error_response ={
+            'Error':{
+                'Code': 'InvalidParameterException',
+                'Message': 'An error ocurred'
+            }
+        }
+
+        mock_client_instance.admin_initiate_auth.side_effect = ClientError(error_response, 'admin_initiate_auth')
+
+        result = app.lambda_handler(mock_success, None)
+        status_code = result["statusCode"]
+        self.assertEqual(status_code, 400)
+
+    @patch("newPassword.app.boto3.client")
+    def test_error_500(self, mock_boto_client):
+        mock_client_instance = mock_boto_client.return_value
+        error_response ={
+            'Error':{
+                'Code': 'InvalidParameterException',
+                'Message': 'An error ocurred'
+            }
+        }
+
+        mock_client_instance.admin_initiate_auth.side_effect = Exception(error_response, 'admin_initiate_auth')
+
+        result = app.lambda_handler(mock_success, None)
+        status_code = result["statusCode"]
+        self.assertEqual(status_code, 500)

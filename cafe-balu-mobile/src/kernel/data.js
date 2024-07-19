2f9f591b702736abec1e9fbde691a59a2f080a65
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-07-19 11:42:03 -0600

    [UPDATE] get sales by month and year

--- cafe-balu-mobile/src/kernel/data.js
+++ cafe-balu-mobile/src/kernel/data.js
@@ -487,3 +487,97 @@ export const salesPerDay =[
         ]
     }
 ]
+
+export const monthsList =[
+    {
+        "id": 1,
+        "name": "Enero"
+    },
+    {
+        "id": 2,
+        "name": "Febrero"
+    },
+    {
+        "id": 3,
+        "name": "Marzo"
+    },
+    {
+        "id": 4,
+        "name": "Abril"
+    },
+    {
+        "id": 5,
+        "name": "Mayo"
+    },
+    {
+        "id": 6,
+        "name": "Junio"
+    },
+    {
+        "id": 7,
+        "name": "Julio"
+    },
+    {
+        "id": 8,
+        "name": "Agosto"
+    },
+    {
+        "id": 9,
+        "name": "Septiembre"
+    },
+    {
+        "id": 10,
+        "name": "Octubre"
+    },
+    {
+        "id": 11,
+        "name": "Noviembre"
+    },
+    {
+        "id": 12,
+        "name": "Diciembre"
+    }
+]
+
+export const yearsList =[
+    {
+        "id": 2021,
+        "name": 2021
+    },
+    {
+        "id": 2022,
+        "name": 2022
+    },
+    {
+        "id": 2023,
+        "name": 2023
+    },
+    {
+        "id": 2024,
+        "name": 2024
+    },
+    {
+        "id": 2025,
+        "name": 2025
+    },
+    {
+        "id": 2026,
+        "name": 2026
+    },
+    {
+        "id": 2027,
+        "name": 2027
+    },
+    {
+        "id": 2028,
+        "name": 2028
+    },
+    {
+        "id": 2029,
+        "name": 2029
+    },
+    {
+        "id": 2030,
+        "name": 2030
+    }
+]

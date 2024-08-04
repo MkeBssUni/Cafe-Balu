c7db66cfbe9d1a451a61c0828a7172716399996d
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-08-04 10:09:30 -0600

    [UPDATE] added get secrets

--- cafe-balu-back/tests/unit/test_app_get_products.py
+++ cafe-balu-back/tests/unit/test_app_get_products.py
@@ -1,33 +1,52 @@
 import unittest
 import json
 from get_products import app
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
-mock_success_all ={
+mock_success_all = {
     "pathParameters": {
         "status": 0
     }
 }
 
-mock_success_active ={
+mock_success_active = {
     "pathParameters": {
         "status": 1
     }
 }
 
-mock_invalid_status ={
-    "pathParameters":{
+mock_invalid_status = {
+    "pathParameters": {
         "status": 12
     }
 }
 
-mock_internal_error ={
-    "pathParameters":{
+mock_internal_error = {
+    "pathParameters": {
         "status": 1
     }
 }
 
 class TestUpdateCategory(unittest.TestCase):
+
+    @patch("get_products.app.get_secret")
+    def setUp(self, mock_get_secret):
+        mock_get_secret.return_value = {
+            "host": "database-cafe-balu.cziym6ii4nn7.us-east-2.rds.amazonaws.com",
+            "username": "baluroot",
+            "password": "baluroot",
+            "dbname": "cafe_balu"
+        }
+
+    def test_success(self):
+        result = app.lambda_handler(mock_success_all, None)
+        print(result)
+        status_code = result["statusCode"]
+        self.assertEqual(status_code, 200)
+        body = json.loads(result["body"])
+        self.assertIn("message", body)
+        self.assertEqual(body["message"], "PRODUCTS_FETCHED")
+
     @patch("get_products.app.get_all_products")
     def test_get_all_products_success(self, mock_get_all_products):
         mock_get_all_products.return_value = [
@@ -86,7 +105,6 @@ class TestUpdateCategory(unittest.TestCase):
 
     @patch("get_products.app.get_all_products")
     def test_get_all_products_internal_error(self, mock_get_all_products):
-
         mock_get_all_products.side_effect = Exception('Error')
         result = app.lambda_handler(mock_internal_error, None)
         print(result)
@@ -108,3 +126,6 @@ class TestUpdateCategory(unittest.TestCase):
     def test_decimal_to_float_invalid_type(self):
         with self.assertRaises(TypeError):
             app.decimal_to_float("string")
+
+if __name__ == "__main__":
+    unittest.main()

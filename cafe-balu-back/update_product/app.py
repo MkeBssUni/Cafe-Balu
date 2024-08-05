70ddeb59183885e34a072a78d4e884ed8b2daef8
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-08-05 09:44:09 -0600

    [UPDATE] add description fiel to service

--- cafe-balu-back/update_product/app.py
+++ cafe-balu-back/update_product/app.py
@@ -1,8 +1,9 @@
 import json
 import pymysql
-import logging
 import re
 import boto3
+import uuid
+import base64
 from botocore.exceptions import ClientError
 
 def get_secret():
@@ -35,9 +36,17 @@ rds_user = secrets["username"]
 rds_password = secrets["password"]
 rds_db = secrets["dbname"]
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+bucket_name = "cafe-balu-images"
+s3 = boto3.client('s3')
 
+def upload_image_to_s3(base64_data):
+    # Remover el prefijo de la cadena base64
+    base64_data = base64_data.split(",")[1]
+    binary_data = base64.b64decode(base64_data)
+    file_name = f"images/{uuid.uuid4()}.jpg"
+    s3.put_object(Bucket=bucket_name, Key=file_name, Body=binary_data, ContentType='image/jpeg')
+    s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
+    return s3_url
 
 def lambda_handler(event, __):
     headers = {
@@ -59,10 +68,18 @@ def lambda_handler(event, __):
             }
 
         if 'body' not in event:
-            logger.error("Request body not found in the event")
             raise KeyError('body')
 
-        body = json.loads(event['body'])
+        try:
+            body = json.loads(event['body'])
+        except json.JSONDecodeError as e:
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "INVALID_JSON_FORMAT"
+                }),
+            }
 
         product_id = body.get('id')
         name = body.get('name')
@@ -71,13 +88,90 @@ def lambda_handler(event, __):
         status = body.get('status')
         image = body.get('image')
         category_id = body.get('category_id')
+        description = "Sin descripción"
 
-        if not product_id or not name or not stock or not price or not status or not image or not category_id:
+        if 'description' in body:
+            description = body.get('description')
+
+        if len(description) > 255:
+            return {
+                "statusCode": 413,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "DESCRIPTION_TOO_LONG"
+                }),
+            }
+
+        if description is None:
+            description = "Sin descripción"
+
+        # Validar campos faltantes: 'product_id', 'name', 'stock', 'price', 'status', 'image', 'category_id'
+        missing_fields = [field for field in ['id', 'name', 'stock', 'price', 'status', 'image', 'category_id'] if body.get(field) is None]
+
+        if missing_fields:
             return {
                 "statusCode": 400,
                 "headers": headers,
                 "body": json.dumps({
                     "message": "MISSING_FIELDS",
+                    "missing_fields": missing_fields
+                }),
+            }
+
+        # Validar 'name': debe ser una cadena no vacía y sin caracteres inválidos
+        if not isinstance(name, str) or not name.strip() or not re.match(r'^[\w\s.-]+$', name):
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "INVALID_NAME"
+                }),
+            }
+
+        # Validar 'stock': debe ser un entero mayor o igual a cero
+        if not isinstance(stock, int) or stock < 0:
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "INVALID_STOCK"
+                }),
+            }
+
+        # Validar 'price': debe ser un número mayor a cero
+        if not isinstance(price, (int, float)) or price <= 0:
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "INVALID_PRICE"
+                }),
+            }
+
+        # Validar 'category_id': debe ser un entero positivo
+        if not isinstance(category_id, int) or category_id <= 0:
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "INVALID_CATEGORY_ID"
+                }),
+            }
+        if not category_exists(category_id):
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "CATEGORY_NOT_FOUND"
+                }),
+            }
+
+        if product_exists_in_category(category_id, name, product_id):
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "PRODUCT_EXISTS"
                 }),
             }
 
@@ -86,20 +180,14 @@ def lambda_handler(event, __):
                 "statusCode": 400,
                 "headers": headers,
                 "body": json.dumps({
-                    "message": "INVALID_IMAGE",
+                    "message": "INVALID_IMAGE"
                 }),
             }
 
-        if not category_exists(category_id):
-            return {
-                "statusCode": 400,
-                "headers": headers,
-                "body": json.dumps({
-                    "message": "CATEGORY_NOT_FOUND",
-                }),
-            }
+        # Subir imagen a S3 y obtener la URL
+        image_url = upload_image_to_s3(image)
 
-        update_product(product_id, name, stock, price, status, image, category_id)
+        update_product(product_id, name, stock, price, status, image_url, category_id, description)
         return {
             "statusCode": 200,
             "headers": headers,
@@ -116,6 +204,15 @@ def lambda_handler(event, __):
                 "error": str(e)
             }),
         }
+    except pymysql.MySQLError as e:
+        return {
+            "statusCode": 500,
+            "headers": headers,
+            "body": json.dumps({
+                "message": "DATABASE_ERROR",
+                "error": str(e)
+            }),
+        }
     except Exception as e:
         return {
             "statusCode": 500,
@@ -126,22 +223,18 @@ def lambda_handler(event, __):
             }),
         }
 
-
-def update_product(product_id, name, stock, price, status, image, category_id):
+def update_product(product_id, name, stock, price, status, image_url, category_id, description):
     connection = pymysql.connect(host=rds_host, user=rds_user, password=rds_password, db=rds_db)
-    print(connection)
     try:
         cursor = connection.cursor()
         cursor.execute("""
-                UPDATE products 
-                SET name=%s, stock=%s, price=%s, status=%s, image=%s, category_id=%s 
-                WHERE id=%s
-            """, (name, stock, price, status, image, category_id, product_id))
+            UPDATE products 
+            SET name=%s, stock=%s, price=%s, status=%s, image=%s, category_id=%s, description=%s 
+            WHERE id=%s
+        """, (name, stock, price, status, image_url, category_id, description, product_id))
         connection.commit()
-        logger.info("Product updated successfully with id=%s", product_id)
     except Exception as e:
-        logger.error("Database update error: %s", str(e))
-        raise
+        raise e
     finally:
         connection.close()
 
@@ -151,24 +244,26 @@ def category_exists(category_id):
         cursor = connection.cursor()
         cursor.execute("SELECT COUNT(*) FROM categories WHERE id = %s", (category_id,))
         connection.commit()
-        return cursor.fetchone()[0] > 0
-
+        result = cursor.fetchone()
+        if result is None:
+            return False
+        return result[0] > 0
     except Exception as e:
-        logger.error("Database select error: %s", str(e))
         raise e
     finally:
         connection.close()
 
-def product_exists_in_category(category_id, name,product_id):
+def product_exists_in_category(category_id, name, product_id):
     connection = pymysql.connect(host=rds_host, user=rds_user, password=rds_password, db=rds_db)
     try:
         cursor = connection.cursor()
-        cursor.execute("SELECT COUNT(*) FROM products WHERE category_id = %s AND lower(name) = %s and id != %s", (category_id, name.lower(), product_id))
+        cursor.execute("SELECT COUNT(*) FROM products WHERE category_id = %s AND lower(name) = %s AND id != %s", (category_id, name.lower(), product_id))
         connection.commit()
-        return cursor.fetchone()[0] > 0
-
+        result = cursor.fetchone()
+        if result is None:
+            return False
+        return result[0] > 0
     except Exception as e:
-        logger.error("Database select error: %s", str(e))
         raise e
     finally:
         connection.close()
